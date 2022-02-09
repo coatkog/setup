a9afcff13761bff39a06c4c847ee57a3644ff59a
Aleksandar Milanov <coatkog@gmail.com>
2022-02-09 13:57:30 +0100

    Fixed creating the symlink for tilix.

--- ubuntu/tilix.sh
+++ ubuntu/tilix.sh
@@ -8,7 +8,7 @@ dconf load /com/gexperts/Tilix/ < tilix.dconf
 ##### Terminal rc ##############################################################
 ### Tilix not being able to persist current directory when opening new terminal
 
-sudo ln -s /etc/profile.d/vte-2.91.sh /etc/profile.d/vte.sh
+sudo ln -s /etc/profile.d/vte.sh /etc/profile.d/vte-2.91.sh
 
 echo "Update .zshrc"
 echo 'if [ $TILIX_ID ] || [ $VTE_VERSION ]; then'
